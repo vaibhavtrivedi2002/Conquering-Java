fa83fdc3dd2b437be92aabda0a4f408592249ba5
Vaibhav Trivedi <123184825+vaibhavtrivedi2002@users.noreply.github.com>
2024-03-05 09:05:18 +0530

    Add files via upload

---
+++ table.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class table {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Enter the number:");
+       int n = sc.nextInt(); 
+
+for(int i=1;i<11;i++){
+    System.out.println(i*n);
+}
+}
+
+    }
+
