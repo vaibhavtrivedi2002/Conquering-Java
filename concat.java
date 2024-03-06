ca252a589e5db3804fbcac71b03a100ba90b3f7e
Vaibhav Trivedi <123184825+vaibhavtrivedi2002@users.noreply.github.com>
2024-03-06 12:57:39 +0530

    Add files via upload

---
+++ concat.java
@@ -0,0 +1,8 @@
+public class concat {
+  public static void main(String[] args) {
+    String shabd1 = "hello";
+    String shabd2 = "world";
+    String concatenatedString = shabd1+ shabd2;
+    System.out.println("The concatenated string is: " + concatenatedString);
+  }
+}
