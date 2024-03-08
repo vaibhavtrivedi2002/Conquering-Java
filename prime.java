d597be91d69a5380283b15d7cb01a3bd278c593f
Vaibhav Trivedi <123184825+vaibhavtrivedi2002@users.noreply.github.com>
2024-03-08 08:30:26 +0530

    Add files via upload

---
+++ prime.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class prime {
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int num;
+        System.out.println("Enter a number: ");
+        Scanner scanner = new Scanner(System.in);
+        num = scanner.nextInt();
+
+        if (isPrime(num)) {
+            System.out.println(num + " is a prime number");
+        } else {
+            System.out.println(num + " is not a prime number");
+        }
+    }
+}
