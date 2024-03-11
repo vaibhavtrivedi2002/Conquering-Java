877ed88425587ae4dcf384f073a4fc46ef924408
Vaibhav Trivedi <123184825+vaibhavtrivedi2002@users.noreply.github.com>
2024-03-11 17:39:42 +0530

    Add files via upload

---
+++ count.java
@@ -0,0 +1,14 @@
+public class count {
+
+    public static void main(String[] args) {
+      // Initialize array
+      int[] arr = {1, 2, 3, 4, 5};
+  
+      // Get the number of elements using length
+      int elementsCount = arr.length;
+  
+      // Print the number of elements
+      System.out.println("Number of elements present in the array: " + elementsCount);
+    }
+  }
+  
