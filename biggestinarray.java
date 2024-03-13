f2d4946a78e403e5ff3579e3374f1dc0a309f4e8
Vaibhav Trivedi <123184825+vaibhavtrivedi2002@users.noreply.github.com>
2024-03-13 21:03:16 +0530

    Add files via upload

---
+++ biggestinarray.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+public class biggestinarray{
+    public static void main(String[] args) {
+        int[] array= {22,25,78,65,97};
+        int largest= Arrays.stream(array).max().getAsInt();
+        System.out.println("The largest element in the array is:"+largest);
+    }
+    
+}
