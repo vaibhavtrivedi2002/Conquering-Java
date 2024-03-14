71439cabea59c0e4fd7f8147631d5036cec5d946
Vaibhav Trivedi <123184825+vaibhavtrivedi2002@users.noreply.github.com>
2024-03-14 19:13:46 +0530

    Add files via upload

---
+++ lessgreat.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class lessgreat {
+    public static void main(String[] args) {
+        Scanner likh = new Scanner(System.in);
+        
+        System.out.println("Enter the value of a:");
+        int a= likh.nextInt();
+
+        System.out.println("Enter the value of b:");
+        int b= likh.nextInt();
+
+        if(a==b){
+            System.out.println("a and b are equal");
+        }else if(a>b){
+                System.out.println("a is greater than b");
+            }else{
+                System.out.println("b is greater than a");
+            }
+        }
+    }
+
