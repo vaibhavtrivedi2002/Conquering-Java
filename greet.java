cfd72af164045294eb07c3bc9a1fd6b9fc1b3e7f
Vaibhav Trivedi <123184825+vaibhavtrivedi2002@users.noreply.github.com>
2024-03-15 14:08:26 +0530

    Add files via upload

---
+++ greet.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class greet {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int press = scanner.nextInt();
+        //if(press == 1){
+            //System.out.println("Namaste");
+        //}else if(press == 2){
+                //System.out.println("Hello");
+            //}else if(press ==3){
+                //System.out.println("Bonjour");
+            //}
+
+            switch(press){
+                case 1: System.out.println("Namaste");
+                break;
+                case 2: System.out.println("Hello");
+                break;
+                case 3: System.out.println("Bonjour");
+                break;
+                default: System.out.println("No Result");
+                break;
+
+            }
+    }
+    
+}
