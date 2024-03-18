614452751e96edb8cd88f7c32c6a09bac9de967c
Vaibhav Trivedi <123184825+vaibhavtrivedi2002@users.noreply.github.com>
2024-03-18 20:31:18 +0530

    Add files via upload

---
+++ bazinga.java
@@ -0,0 +1,9 @@
+public class bazinga {
+  public static void main(String[] args) {
+    // if (10%2 ==0){
+    // System.out.pritnln("bazinga");
+    // }
+    if (10 % 2 == 0)
+      System.out.println("(bazinga)");
+  }
+}
