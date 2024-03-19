19903667b36abc6ac8db77c3f106ac8cfdd26147
Vaibhav Trivedi <123184825+vaibhavtrivedi2002@users.noreply.github.com>
2024-03-19 20:29:26 +0530

    Add files via upload

---
+++ calculate.java
@@ -0,0 +1,8 @@
+public class calculate {
+    public static void main(String[] args) {
+        int a=10;
+        int b=5;
+        int sol= (a*b)/(a-b);
+        System.out.println(sol);
+    }
+}
