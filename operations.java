1671a5faea7ff4b84e9b8693b875b1cb3afe8570
Vaibhav Trivedi <123184825+vaibhavtrivedi2002@users.noreply.github.com>
2024-03-25 22:09:45 +0530

    Add files via upload

---
+++ operations.java
@@ -0,0 +1,15 @@
+//this program performs multiple operations through various functions
+public class operations {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int add = a + b;
+        int subtract = b - a;
+        int multiply = a * b;
+        int division = b / a;
+        System.out.println(add);
+        System.out.println(subtract);
+        System.out.println(multiply);
+        System.out.println(division);
+    }
+}
