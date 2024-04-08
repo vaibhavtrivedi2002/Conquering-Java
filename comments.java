c54b488399e4c07e4882841967107bd6aef447dc
Vaibhav Trivedi <123184825+vaibhavtrivedi2002@users.noreply.github.com>
2024-04-08 16:50:03 +0530

    Create comments.java

---
+++ comments.java
@@ -0,0 +1,7 @@
+//this program contains comments
+public class comments{
+  public static void main (string[] args){
+    //this is a single line comment 
+    /*this is a 
+    multi line comment*/
+  System.out.print("Java Comments");
