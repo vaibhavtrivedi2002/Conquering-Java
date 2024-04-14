901422a0d5e7d5cd46522612969dd954eed1f316
Vaibhav Trivedi <123184825+vaibhavtrivedi2002@users.noreply.github.com>
2024-04-14 20:51:12 +0530

    Create switchcases.java

---
+++ switchcases.java
@@ -0,0 +1,35 @@
+//this program shows use of switch case
+public class switchcases {
+  public static void main(String[] args) {
+
+    int number = 44;
+    String size;
+
+    // switch statement to check size
+    switch (number) {
+
+      case 29:
+        size = "Small";
+        break;
+
+      case 42:
+        size = "Medium";
+        break;
+
+      // match the value of week
+      case 44:
+        size = "Large";
+        break;
+
+      case 48:
+        size = "Extra Large";
+        break;
+      
+      default:
+        size = "Unknown";
+        break;
+
+    }
+    System.out.println("Size: " + size);
+  }
+  }
