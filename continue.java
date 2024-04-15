2601f1d72091307d67dda1e78567f6021f1f5967
Vaibhav Trivedi <123184825+vaibhavtrivedi2002@users.noreply.github.com>
2024-04-15 22:09:00 +0530

    Create continue.java

---
+++ continue.java
@@ -0,0 +1,16 @@
+//this program shows use of continue
+public class continue{
+  public static void main(String[] args) {
+
+    // for loop
+    for (int i = 1; i <= 10; ++i) {
+
+      // if value of i is between 4 and 9
+      // continue is executed
+      if (i > 4 && i < 9) {
+        continue;
+      }
+      System.out.println(i);
+    }
+  }
+}
