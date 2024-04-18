ee9647b34990f177afd44be57f2ceb4ce8ab5c01
Vaibhav Trivedi <123184825+vaibhavtrivedi2002@users.noreply.github.com>
2024-04-18 21:27:53 +0530

    Create sqrt.java

---
+++ sqrt.java
@@ -0,0 +1,8 @@
+//this program prints the square root of the given number
+public class Main {
+  public static void main(String[] args) {
+    
+    // using the sqrt() method
+    System.out.print("Square root of 4 is: " + Math.sqrt(4));
+  }
+}
