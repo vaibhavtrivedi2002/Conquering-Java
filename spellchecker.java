b90c9480dcfec6e76bc991ed245202e27777fba3
Vaibhav Trivedi <123184825+vaibhavtrivedi2002@users.noreply.github.com>
2024-04-21 20:17:57 +0530

    Create spellchecker.java

---
+++ spellchecker.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class spellchecker {
+
+    public static void main(String[] args) {
+
+        String originalWord = "dictionary"; // Replace with your desired word
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the spelled word: ");
+        String userWord = scanner.nextLine();
+
+        if (originalWord.equalsIgnoreCase(userWord)) {
+            System.out.println("Correct spelling");
+        } else {
+            System.out.println("Incorrect spelling");
+        }
+    }
+}
