7eee771809e2e891d3e04ff95d8160c16e95b736
Vaibhav Trivedi <123184825+vaibhavtrivedi2002@users.noreply.github.com>
2024-04-24 20:25:00 +0530

    Update

---
+++ touppercase.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class touppercase{
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter a string: ");
+    String inputString = scanner.nextLine();
+
+    String upperCaseString = inputString.toUpperCase();
+    System.out.println("Uppercase: " + upperCaseString);
+  }
+}
