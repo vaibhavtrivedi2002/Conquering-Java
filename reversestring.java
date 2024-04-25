bae6e2603c1ea19b53b143c7cacfff2d319dd477
Vaibhav Trivedi <123184825+vaibhavtrivedi2002@users.noreply.github.com>
2024-04-25 21:11:13 +0530

    Update

---
+++ reversestring.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class reversestring {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter a string: ");
+
+    String input = scanner.nextLine();
+    String reversed = new StringBuilder(input).reverse().toString();
+
+    System.out.println("Reversed String: " + reversed);
+  }
+}
