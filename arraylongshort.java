7892a63d159db8530ba7ec7fc7fa7c53d4b50aac
Vaibhav Trivedi <123184825+vaibhavtrivedi2002@users.noreply.github.com>
2024-04-28 21:59:48 +0530

    Update

---
+++ arraylongshort.java
@@ -0,0 +1,11 @@
+class arraylongshort{
+  public static void main(String[] args) {
+    int[] t = {1, 2, 3};
+
+    if (t.length > 3) {
+      System.out.println("Longer");
+    } else {
+      System.out.println("Shorter");
+    }
+  }
+}
