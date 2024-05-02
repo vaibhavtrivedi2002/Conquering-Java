b965a8382ee0a85bbc3ddeed6c3a299ed3d1c7bf
Vaibhav Trivedi <123184825+vaibhavtrivedi2002@users.noreply.github.com>
2024-05-02 22:33:43 +0530

    Update

---
+++ ifstate.java
@@ -0,0 +1,10 @@
+public class ifstate{
+public static void main (String []args){
+int number=10;
+if(number<0){
+System.out.print("The number is negative");
+}else{
+System.out.print("Statement outside if block");
+}
+}
+}
