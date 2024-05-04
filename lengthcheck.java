d49fa0b40d3bdeeaa5536ae83d99c9c70d72f82a
Vaibhav Trivedi <123184825+vaibhavtrivedi2002@users.noreply.github.com>
2024-05-04 20:30:05 +0530

    Update

---
+++ lengthcheck.java
@@ -0,0 +1,11 @@
+public class lengthcheck{
+    public static void main(String[] args) {
+        int[] t = {1, 2, 3}; // In Java, we use arrays instead of tuples
+
+        if (t.length > 3) {
+            System.out.println("Longer");
+        } else {
+            System.out.println("Shorter");
+        }
+    }
+}
