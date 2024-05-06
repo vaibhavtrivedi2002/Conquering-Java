cfb5307893a13ad5893a2cb99cbced722d637552
Vaibhav Trivedi <123184825+vaibhavtrivedi2002@users.noreply.github.com>
2024-05-06 22:21:24 +0530

    Update

---
+++ break.java
@@ -0,0 +1,14 @@
+public class break{
+    public static void main(String[] args) {
+      
+        // for loop
+        for (int i = 1; i <= 10; ++i) {
+
+            // if the value of i is 5 the loop terminates  
+            if (i == 5) {
+                break;
+            }      
+            System.out.println(i);
+        }   
+    }
+}
