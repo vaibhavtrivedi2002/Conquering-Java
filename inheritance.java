47c4e93983fb212c9adb3f205380116308a65f79
Vaibhav Trivedi <123184825+vaibhavtrivedi2002@users.noreply.github.com>
2024-05-17 21:37:21 +0530

    Update

---
+++ inheritance.java
@@ -0,0 +1,22 @@
+class Animal {
+  String name;
+
+  public void eat() {
+    System.out.println("I can eat");
+  }
+}
+
+class Dog extends Animal {
+  public void display() {
+    System.out.println("My name is " + name);
+  }
+}
+
+public class inheritance {
+  public static void main(String[] args) {
+    Dog labrador = new Dog();
+    labrador.name = "Rohu";
+    labrador.display();
+    labrador.eat();
+  }
+}
