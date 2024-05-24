c91ad18e2f76ba3f608378b470e52162620cb11f
Vaibhav Trivedi <123184825+vaibhavtrivedi2002@users.noreply.github.com>
2024-05-24 20:19:33 +0530

    Update

---
+++ stringlength.java
@@ -0,0 +1,8 @@
+public class stringlength{
+
+    public static void main(String[] args) {
+        String str = "Hello, world!";
+        int length = str.length();
+        System.out.println("The length of the string is: " + length);
+    }
+}
