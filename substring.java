9883b825d9caa0db0ed5d1b0fdafa22a71912df7
Vaibhav Trivedi <123184825+vaibhavtrivedi2002@users.noreply.github.com>
2024-05-25 22:08:11 +0530

    Update

---
+++ substring.java
@@ -0,0 +1,8 @@
+public class substring{
+
+    public static void main(String[] args) {
+        String str = "Java Programming";
+        String substring = str.substring(7, 12);  // "world"
+        System.out.println("The substring from index 7 to 11 is: " + substring);
+    }
+}
