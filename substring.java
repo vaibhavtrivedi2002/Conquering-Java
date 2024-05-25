41dba6ef1467992870ae46653b9e9e181bd7ceb0
Vaibhav Trivedi <123184825+vaibhavtrivedi2002@users.noreply.github.com>
2024-05-25 22:08:42 +0530

    Update substring.java

--- substring.java
+++ substring.java
@@ -2,7 +2,7 @@ public class substring{
 
     public static void main(String[] args) {
         String str = "Java Programming";
-        String substring = str.substring(7, 12);  // "world"
+        String substring = str.substring(7, 12); 
         System.out.println("The substring from index 7 to 11 is: " + substring);
     }
 }
