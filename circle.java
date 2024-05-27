e3647396aeb9699e4983e26fb22301a4d294ee69
Vaibhav Trivedi <123184825+vaibhavtrivedi2002@users.noreply.github.com>
2024-05-27 22:12:57 +0530

    Update

---
+++ circle.java
@@ -0,0 +1,8 @@
+public class circle{
+
+  public static void main(String[] args) {
+    double radius = 5;
+    double area = Math.PI * radius * radius;
+    System.out.println("Area of circle with radius " + radius + " is: " + area);
+  }
+}
