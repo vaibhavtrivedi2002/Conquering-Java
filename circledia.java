37c8b7f1ba774b42909c54760f38e29dc670efd8
Vaibhav Trivedi <123184825+vaibhavtrivedi2002@users.noreply.github.com>
2024-05-29 20:31:25 +0530

    Update

---
+++ circledia.java
@@ -0,0 +1,9 @@
+public class circledia{
+
+    public static final double PI = Math.PI;
+
+    public static void main(String[] args) {
+        System.out.println("Formula for Circle Diameter:");
+        System.out.println("  * Diameter (d): d = 2r");
+    }
+}
