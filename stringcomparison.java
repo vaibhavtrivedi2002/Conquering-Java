aaff9e6d3a0720d6a59b132f7c4563007188d042
Vaibhav Trivedi <123184825+vaibhavtrivedi2002@users.noreply.github.com>
2024-06-10 22:24:07 +0530

    Update

---
+++ stringcomparison.java
@@ -0,0 +1,13 @@
+public class stringcomparison{
+
+  public static void main(String[] args) {
+    String name1 = "Alice";
+    String name2 = "Bob";
+
+    if (name1.equals(name2)) {
+      System.out.println("name1 and name2 are equal");
+    } else {
+      System.out.println("name1 and name2 are not equal");
+    }
+  }
+}
