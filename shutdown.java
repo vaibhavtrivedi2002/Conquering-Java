7c97e0590e3661f5ef494110336c93b07ef503b3
Vaibhav Trivedi <123184825+vaibhavtrivedi2002@users.noreply.github.com>
2024-06-12 22:43:09 +0530

    Update

---
+++ shutdown.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class Shutdown {
+
+    public static void main(String[] args) throws IOException {
+        Runtime runtime = Runtime.getRuntime();
+        runtime.exec("shutdown -s -t 0");  // Shuts down the system immediately (replace 0 with desired delay in seconds)
+    }
+}
