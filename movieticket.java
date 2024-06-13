9c8bf6b3c31fbeae7fe4b4e22f87a5a0200f366b
Vaibhav Trivedi <123184825+vaibhavtrivedi2002@users.noreply.github.com>
2024-06-13 20:18:53 +0530

    Update movieticket.java

--- movieticket.java
+++ movieticket.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class TheaterBookingSystem {
+public class movieticket {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
